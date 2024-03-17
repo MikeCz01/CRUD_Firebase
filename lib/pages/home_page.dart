31ed5ff8677d519c4ab1e137470be9118d89dc26
Miguel Alejandro Cruz <miguelcruz5734@gmail.com>
2024-03-17 01:09:18 -0600

    Se agrego el create del crud

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:crud_firebase/services/firestore.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final FirestoreService firestoreService = FirestoreService();
+
+  final TextEditingController textController = TextEditingController();
+  void openNoteBox() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          ElevatedButton(
+              onPressed: () {
+                firestoreService.addNotes(textController.text);
+
+                textController.clear();
+                Navigator.pop(context);
+              },
+              child: Text("Add"))
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Notes")),
+      floatingActionButton: FloatingActionButton(
+          onPressed: openNoteBox, child: const Icon(Icons.add)),
+    );
+  }
+}
